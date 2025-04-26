b89e1df9af213bd24d00a1133f4dd929db695f2a
Durgesh Gangaram Deore <durgeshdeore9674@gmail.com>
2025-04-26 22:46:29 +0530

    DB connnected

--- server/server.js
+++ server/server.js
@@ -3,13 +3,21 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import 'dotenv/config';
 
+import connectDB from "./config/mongodb.js";
+import authRouter from "./routes/authRoutes.js";
+
 const app = express();
 const port = process.env.port || 3030;
+connectDB();
+
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({credentials: true}));
 
+//API endpoints
 app.get('/',(req,res) => res.send('API working !!'));
+app.use('/api/auth' ,authRouter)
+
 
 app.listen(port, ()=> console.log(`server running on ${port}`));
